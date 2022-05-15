c948a476cfdff4bc45b5a0cf07ffef80ef05674b
Charles-Hong520 <chong039@ucr.edu>
2022-05-15 10:50:13 -0700

    added main ui

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+int main() {
+	int featureNumber, algorithmNumber;
+
+	cout<<"Welcome to Charles Hong's Feature Selection Algorithm.\n"
+	cout<<"Please enter total number of features: ";
+	cin>>featureNumber;
+	cout<<"Type the number of the algorithm you want to run.\n";
+	cout<<"1. Forward Selection\n";
+	cout<<"2. Backward Elimination\n";
+	cout<<"3. Charles's Special Algorithm\n";
+	cin>>algorithmNumber;
+
+
+	
+}
